b668b78bd7d5a01fce3802154079f3cec85c401d
James <wilfongjt@gmail.com>
2024-04-22 07:43:22 -0400

    Init

---
+++ bin/md2.py
@@ -0,0 +1,13 @@
+## md2 new project
+
+##* Create branch folder when folder not found
+
+##* Clone repo when repo not found
+
+##* Create repo/.env file when file not found
+
+##* Create bin folder when folder not found
+
+##* Create scripts folder when folder not found
+
+##* Create scripts/git.rebase.sh file when file not found
