bd45734ada03cd6563cc844316efc05b9d117f86
James <wilfongjt@gmail.com>
2024-04-22 10:22:59 -0400

    Init

--- bin/md2.py
+++ bin/md2.py
@@ -1,5 +1,12 @@
 ## md2 new project
 
+##* collect new project values
+
+##* upsert .gitignore set line = '*.env'
+##* upsert .gitignore set line = '.idea/'
+
+# NEW
+
 ##* Create branch folder when folder not found
 
 ##* Clone repo when repo not found
@@ -9,5 +16,7 @@
 ##* Create bin folder when folder not found
 
 ##* Create scripts folder when folder not found
+
+##* Create source folder when folder not found
 
 ##* Create scripts/git.rebase.sh file when file not found
